8d89c4a9e65f49d41acf6d654c89f14b7f0c1a7a
Misha <mihail.nazarov@sustems.de>
2018-04-11 10:47:36 +0300

    done

---
+++ src/components/posts_index.js
@@ -0,0 +1,55 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPosts, deletePost } from "../actions";
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  onDeleteClick(id){
+    this.props.deletePost(id,()=>{
+      console.log('done')
+    })
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            {post.title}
+          </Link>
+          <div
+            className="delete-icon"
+            value={post.id}
+            onClick={()=>this.onDeleteClick(post.id)}
+          >x</div>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts, deletePost })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import{ createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ReduxPromise from "redux-promise";
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import './app.scss';
 
-import PostIndex from './components/post_index';
+import PostIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(reducers, devToolsEnhancer())}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={ PostIndex } />
+        <Switch>
+          <Route path="/posts/new" component={ PostsNew } />
+          <Route path="/posts/:id" component={ PostsShow } />
+          <Route path="/" component={ PostIndex } />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>, document.getElementById('root')
